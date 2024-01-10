1c9dc5914287001ed1aadf2ecbf62c1eaf483902
jofoks <joormillenaar@live.nl>
2024-01-10 21:42:29 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open('README.md') as file:
+    long_description = file.read()
+
+setup(
+    name='pypsola',
+    description='Pitch shifting without duration change',
+    version='0.0.1',
+    author='Joram Millenaar',
+    author_email='joormillenaar@live.nl',
+    url='https://github.com/jofoks/PyPSOLA',
+    install_requires=['numpy'],
+    packages=['pypsola'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=['audio', 'duration', 'pitch', 'music', 'stretch', 'vocode', 'vocals'],
+    license='MIT'
+)
